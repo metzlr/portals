b6cee052cd0af1ed1c3a281f77ecf7068af1d047
Reed Metzler-Gilbertz <reedmg15@gmail.com>
2021-04-12 02:03:17 -0700

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+import { SceneManager } from "./SceneManager.js";
+import Stats from "three/examples/jsm/libs/stats.module";
+
+(function () {
+  const canvas = document.getElementById("main-canvas");
+  const sceneManager = new SceneManager(canvas);
+  const stats = setupStatDisplay();
+  if (stats != undefined) {
+    document.body.appendChild(stats.dom);
+  }
+
+  renderScene();
+
+  function renderScene() {
+    requestAnimationFrame(renderScene);
+    sceneManager.update();
+    if (stats != undefined) {
+      stats.update();
+    }
+  }
+
+  function setupStatDisplay() {
+    let stats = new Stats();
+    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    return stats;
+  }
+})();
